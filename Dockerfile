f70d80d5cb790b643478a6a4b0a0199efb51f84c
Jason Harris <jason.harris438@gmail.com>
2023-03-26 22:21:13 +1100

    added environment setup to setup.sh

--- Dockerfile
+++ Dockerfile
@@ -1,13 +1,6 @@
 # Parent image gcc:12.
 FROM gcc:12
 
-# Install GNU compiler tools, Cmake, the GDB debugger, and GNU scientific lib.
-RUN apt-get -yqq update && \
-    apt-get -yqq install build-essential gdb libtool autoconf unzip wget &&\
-    apt-get -yqq install libgsl-dev
-
-RUN apt remove --purge --auto-remove cmake
-
 # Copy source code.
 COPY stochastic_models ./stochastic_models
 COPY CMakeLists.txt ./
--- setup.sh
+++ setup.sh
@@ -1,9 +1,20 @@
 #! /usr/bin/env bash
 
+# Install system dependencies.
+# GNU compiler tools, Cmake, the GDB debugger,
+# and GNU scientific lib.
+apt-get -yqq update && \
+    apt-get -yqq install build-essential gdb libtool autoconf unzip wget &&\
+    apt-get -yqq install libgsl-dev
+
+# Remove any existing cmake installation.
+apt remove --purge --auto-remove cmake
+
+# Set cmake configuration variables.
 version=3.26
 build=1
 
-## don't modify from here
+# Install cmake.
 mkdir ~/temp
 cd ~/temp
 wget https://cmake.org/files/v$version/cmake-$version.$build.tar.gz
