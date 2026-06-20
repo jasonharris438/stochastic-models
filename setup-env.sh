3c448f3e27ed4edaeb52c9bb2d0f2c266d5adcbe
jasonharris438 <jason.harris438@gmail.com>
2026-06-20 23:01:35 +1000

    gcc-15 not availale on apt

--- setup-env.sh
+++ setup-env.sh
@@ -10,9 +10,9 @@ apt-get update && apt-get install -y --no-install-recommends \
 	ca-certificates \
 	libc6 \
 	libssl-dev \
-	gcc-15 \
-	g++-15
+	gcc-14 \
+	g++-14
 
 # Update default links.
-update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-15 100
-update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-15 100
+update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-14 100
+update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-14 100
